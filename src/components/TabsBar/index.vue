0f8dc0c132df9f0fc7505c4bc101c08729d7491f
嘉林 <919773897@qq.com>
2022-03-10 16:03:48 +0800

    Tabas文件 添加各自该有class

--- src/components/TabsBar/index.vue
+++ src/components/TabsBar/index.vue
@@ -1,9 +1,19 @@
 <template>
   <div>
     <nav class="Tabs_nav">
-      <router-link v-for="(item, index) in title" :key="index" :to="item.to">{{
-        item.title
-      }}</router-link>
+      <div class="Tabs_nav_div">
+        <router-link
+          v-for="(item, index) in title"
+          :key="index"
+          :class="{
+            active: activeClass === index,
+            bacolod: back === 1,
+          }"
+          @click.native="change(index)"
+          :to="item.to"
+          >{{ item.title }}</router-link
+        >
+      </div>
     </nav>
     <slot></slot>
   </div>
@@ -13,17 +23,70 @@
 export default {
   name: "TabsBar",
   props: {
-    title: Object,
+    title: [Object, Array],
   },
-  created() {
-    console.log(this.title);
+  data() {
+    return {
+      activeClass: 0,
+      back: 0,
+    };
+  },
+  created() {},
+  watch: {
+    $route: {
+      handler: function (route) {
+        if (route.fullPath == "/homeBar/recommendBar") {
+          this.back = 1;
+        } else {
+          this.back = 0;
+          console.log(this.$route.fullPath);
+        }
+      },
+      immediate: true,
+    },
+  },
+  methods: {
+    change(index) {
+      this.activeClass = index;
+      console.log(this.activeClass);
+    },
   },
 };
 </script>
 
 <style lang="scss" scoped>
 .Tabs_nav {
-  display: flex;
-  justify-content: space-between;
+  width: 100%;
+  overflow: scroll;
+  padding: 10px 0;
+  // 去除横滚样式
+  &::-webkit-scrollbar {
+    display: none;
+  }
+
+  &_div {
+    width: 150%;
+    display: flex;
+    justify-content: space-between;
+    background: #fff;
+    a {
+      padding: 10px;
+      text-decoration: none;
+      color: #000;
+      text-align: center;
+      &.active {
+        color: #f00;
+        font-size: 20px;
+        border-bottom: 2px solid rgb(255, 0, 0);
+      }
+      // 路由判断 class
+      &.bacolod {
+        color: aqua;
+        &.active {
+           border-bottom: 2px solid rgb(0, 255, 255);
+        }
+      }
+    }
+  }
 }
 </style>
